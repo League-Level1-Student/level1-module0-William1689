8f3b68dfc593ee09e20382c85c4d512347d6e9fe
league <league@imac-22.attlocal.net>
2023-10-20 16:59:07 -0700

    I did stuff

--- src/_04_gui_from_scratch/_2_jack_in_the_box/JackInABox.java
+++ src/_04_gui_from_scratch/_2_jack_in_the_box/JackInABox.java
@@ -1,27 +1,101 @@
 package _04_gui_from_scratch._2_jack_in_the_box;
 
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 
 public class JackInABox implements ActionListener {
+	JButton button = new JButton();
+	JFrame frame = new JFrame();
+	int num = 0;
 	public void showButton() {
-       JFrame frame = new JFrame();
+      
        frame.setVisible(true);
-       JButton button = new JButton();
        frame.add(button);
        button.setText("Suprise!");
        frame.pack();
        button.addActionListener(this);
        
-   }
+	}                                                                                                                            
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void actionPerformed(ActionEvent arg0) {
+		JButton buttonPressed = (JButton) arg0.getSource();
+		if(buttonPressed == button) {
+			num++;      
+			
+		}
+		if(num==5) {
+			playSound("homer-woohoo.wav");
+			showPicture("jackInTheBox.png");
+			
+		}
 	}
+		// TODO Auto-generated method stub
+	private void showPicture(String fileName) {
+	     try {
+	          JLabel imageLabel = createLabelImage(fileName);
+	          JFrame frame = new JFrame();
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+	private JLabel createLabelImage(String fileName) {
+	     try {
+	          URL imageURL = getClass().getResource(fileName);
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	          } else {
+	               Icon icon = new ImageIcon(imageURL);
+	               JLabel imageLabel = new JLabel(icon);
+	               return imageLabel;
+	          }
+	     } catch (Exception e) {
+	          System.err.println("Could not find image " + fileName);
+	          return new JLabel();
+	     }
+	}
+
+	private void playSound(String soundFile) {
+	     try {
+	          AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+	          sound.play();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
 
--- src/_04_gui_from_scratch/_2_jack_in_the_box/JackInTheBoxRunner.java
+++ src/_04_gui_from_scratch/_2_jack_in_the_box/JackInTheBoxRunner.java
@@ -1,5 +1,12 @@
 package _04_gui_from_scratch._2_jack_in_the_box;
 
-public class JackInTheBoxRunner {
+import _03_gui_with_help._5_duelling_buttons.DuellingButtons;
 
+public class JackInTheBoxRunner {
+	public static void main(String[] args) {
+		new JackInABox().showButton();
+	}
 }
+
+
+
